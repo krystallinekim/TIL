bd04a096323d08730d77cb9ee4cd321b745f2e61
krystallinekim <jskim11070@gmail.com>
2025-06-30 12:38:44 +0900

    SQL DB 생성, Table 생성

---
+++ 3. SQL/01-createDB.sql
@@ -0,0 +1,15 @@
+-- DB 생성
+CREATE DATABASE sample_db;
+
+-- 현재 DB 목록을 확인
+SHOW DATABASES;
+
+-- DB 삭제
+DROP DATABASE sample_db;
+
+-- 수업용 lecture, practice DB 생성
+CREATE DATABASE lecture;
+CREATE DATABASE practice;
+
+-- 특정 DB를 사용하겠다고 선언
+USE lecture;
---
+++ 3. SQL/02-createT.sql
@@ -0,0 +1,6 @@
+CREATE TABLE sample(
+	name VARCHAR(30),
+    age INT
+);
+
+SHOW TABLES;
