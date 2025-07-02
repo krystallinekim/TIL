83d3296ffcf7bd538fd6a4851a3f62948275f7c0
krystallinekim <jskim11070@gmail.com>
2025-07-02 11:39:41 +0900

    number func, 실습7 추가

--- 3.SQL/codes/12-number_func.sql
+++ 3.SQL/codes/12-number_func.sql
@@ -1,6 +1,7 @@
 USE lecture;
 SELECT * FROM dt_demo;
 
+-- 실수(소수점) 함수들
 SELECT
 	name,
     score AS 원점수,	
@@ -10,3 +11,56 @@ SELECT
     FLOOR(score) AS 내림,			-- 소수점 아래를 전부 내림
     TRUNCATE(score, 1) AS 버림		-- 소수점 아래 n번째 자리를 전부 버림 # 내림과 버림의 차이는 음수일때
 FROM dt_demo;
+
+-- 사칙연산
+SELECT
+	10 + 3 		AS 덧셈,
+    10 - 3 		AS 뺄셈,
+    10 * 3 		AS 곱셈,
+    10 / 3 		AS 나눗셈,
+	10 DIV 3 	AS 몫,
+    10 % 3 		AS 나머지,
+    MOD(10,3) 	AS 나머지2,	-- Modulo
+    POWER(10,3)	AS 거듭제곱,	-- Square(^2), Cube(^3), 나머지는 Power
+    SQRT(10)	AS 루트,		-- Square Root
+    ABS(-10)	AS 절댓값;	-- Absolute
+    
+    
+    
+-- 조건문 (IF)
+SELECT
+	name,
+    score,
+    IF(score >= 80, '우수', '보통') AS 평가		-- IF는 TRUE/FALSE인 경우에 적합 / 늘어나면 힘들어진다
+FROM dt_demo;
+
+-- 조건문 (CASE)
+SELECT
+	name,
+    score,
+    CASE
+		WHEN score >= 90 THEN 'A'	-- CASE는 다양한 조건을 걸 수 있음
+        WHEN score >= 80 THEN 'B'	-- 위에서부터 내려가면서 해당사항 있으면 바로 값을 준다
+        WHEN score >= 70 THEN 'C'	-- 위로 갈수록 좁은 조건, 아래로 갈수록 넓은 조건을 줘야함
+        ELSE 'D'					-- ELSE는 나머지 값이 해당되지 않은 경우를 전부 이걸로 처리하겠다는것
+        END AS Grade
+FROM dt_demo;
+
+-- IF와 CASE는 조건이 2개일 경우 같은 일을 한다
+SELECT
+	id,
+    name,
+    id % 2 AS 나머지,
+	CASE
+		WHEN id % 2 = 0 THEN '짝수'
+        ELSE '홀수'
+	END AS 홀짝,
+    IF (id %2 = 1, '홀수', '짝수') AS 홀짝2
+FROM dt_demo;
+
+-- NULL
+SELECT
+	name,
+    IFNULL(score,0) AS 점수
+FROM dt_demo;
+
--- 3.SQL/codes/p07.sql
+++ 3.SQL/codes/p07.sql
@@ -1,1 +1,31 @@
 USE practice;
+CREATE TABLE dt_demo_practice AS SELECT * FROM lecture.dt_demo_practice;
+SELECT * FROM dt_demo_practice;
+
+
+-- 종합 정보 표시
+-- id / name / nickname (NULL=미설정) / 출생년도 (19xx년생) / 나이 / 점수 (반올림-소수1, NULL=0), 등급 (A>=90,B>=80,C>=70,나머지는 D), 상태(활성/비활성), 연령대 (미성년 - 19 - 청년 - 30 - 중년 - 50 - 장년 - 64 - 노년)
+
+SELECT
+	id AS ID,
+    name AS 이름,
+    IFNULL(nickname,'미설정') AS 닉네임,
+    DATE_FORMAT(birth,'%Y년생') AS 출생년도,
+    TIMESTAMPDIFF(YEAR,birth,NOW()) AS 나이,
+    IFNULL(ROUND(score,1),0) AS 점수,
+    CASE
+		WHEN score >= 90 THEN 'A'
+        WHEN score >= 80 THEN 'B'
+        WHEN score >= 70 THEN 'C'
+        ELSE 'D'
+	END AS 등급,
+    IF(is_active = 1, '활성', '비활성') AS 상태,
+    CASE
+		WHEN TIMESTAMPDIFF(YEAR,birth,NOW()) >= 64 THEN '노년'
+		WHEN TIMESTAMPDIFF(YEAR,birth,NOW()) >= 50 THEN '중년'
+		WHEN TIMESTAMPDIFF(YEAR,birth,NOW()) >= 40 THEN '장년'
+		WHEN TIMESTAMPDIFF(YEAR,birth,NOW()) >= 30 THEN '청장년'
+		WHEN TIMESTAMPDIFF(YEAR,birth,NOW()) >= 19 THEN '청년'
+        ELSE '미성년'
+	END AS 연령대
+FROM dt_demo_practice;
