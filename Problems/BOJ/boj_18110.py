6b36fdc598a506c1f54d767fa2fbfd812274c128
krystallinekim <jskim11070@gmail.com>
2025-09-03 17:42:13 +0900

    sys.stdin.readline() 이용해 시간 줄이기

--- Problems/BOJ/boj_18110.py
+++ Problems/BOJ/boj_18110.py
@@ -1,16 +1,17 @@
-n = int(input())
-if n:
-    lst = []
+import sys
 
-    for _ in range(n):
-        lst.append(int(input()))
-
-    lst.sort()
-    cut = int(n * 0.15 + 0.5)
-    if cut:
-        ans = sum(lst[cut:-cut]) / (n - 2 * cut)
-    else:
-        ans = sum(lst) / n
-    print(int(ans + 0.5))
-else:
+n = int(sys.stdin.readline())
+if n == 0:
     print(0)
+    sys.exit()
+
+lst = [int(sys.stdin.readline()) for _ in range(n)]
+lst.sort()
+
+cut = int(n * 0.15 + 0.5)
+if cut:
+    ans = sum(lst[cut:-cut]) / (n - 2 * cut)
+else:
+    ans = sum(lst) / n
+
+print(int(ans + 0.5))
