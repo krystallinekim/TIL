9a56a13091514eead93b58ee37a768b15b9a7d11
krystallinekim <jskim11070@gmail.com>
2025-09-03 13:19:07 +0900

    수 찾기

---
+++ Problems/BOJ/boj_1920.py
@@ -0,0 +1,9 @@
+n = int(input())
+a = set(map(int, input().split()))
+m = int(input())
+nums = list(map(int, input().split()))
+for num in nums:
+    if num in a:
+        print(1)
+    else:
+        print(0)
