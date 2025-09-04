0c75ec9af6c445291417ff26f79069dd629ec9a5
krystallinekim <jskim11070@gmail.com>
2025-09-04 16:47:08 +0900

    프롬프트 수정

--- LLM_agent/실습/agent.py
+++ LLM_agent/실습/agent.py
@@ -1,12 +1,10 @@
-# base
+# 학습 어시스턴트 에이전트 작성
 import os
-from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
 
 # agent
+from langchain_openai import ChatOpenAI
 from langchain.agents import create_openai_tools_agent, AgentExecutor
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.runnables import RunnablePassthrough, RunnableLambda
 
 # memory
 from langchain.memory import ConversationBufferMemory
@@ -24,9 +22,14 @@ from langchain.tools.retriever import create_retriever_tool
 # 출력
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
+# 에러
 import warnings
 
 warnings.filterwarnings('ignore', category=DeprecationWarning)
+
+# env
+from dotenv import load_dotenv
+
 load_dotenv()
 
 # LLM
@@ -39,9 +42,16 @@ llm = ChatOpenAI(
 
 # 프롬프트
 system_message = f'''
+
 당신은 웹 검색도 가능하고, 사용자가 공부한 내용도 검색할 수 있는 어시스턴트입니다.
 
 사용자는 AI 기반 데이터 분석가 양성 과정에서 SQL, 파이썬, 머신러닝, LLM 등에 대해 공부했습니다.
+
+다음 기준에 맞게 답변을 해주세요.
+
+---
+
+기준:
 
 1. 사용자가 질문한 내용이 공부한 내용과 관련이 있다면, 먼저 rag_search 툴을 사용해서 답변을 생성합니다.
 
@@ -57,7 +67,9 @@ system_message = f'''
 
 7. 답을 모르면, 그냥 모른다고 답합니다.
 
-항상 가장 의미 있는 결과를 정리해서 요점만 한국어로 전달하고, 항상 존댓말을 사용합니다.
+---
+
+항상 가장 의미 있는 결과를 정리해서 요점만 한국어로 전달하고, 항상 존댓말을 사용하세요.
 
 '''
 
@@ -115,7 +127,6 @@ if os.path.exists(vectorstore_path) and os.listdir(vectorstore_path):
 else:
     vectorstore = make_vectorstore()
     
-
 # RAG
 retriever = vectorstore.as_retriever()
 
@@ -144,7 +155,7 @@ agent_executor = AgentExecutor(
 
 # 챗봇 실행
 if __name__ == '__main__':
-    print('=== 챗봇을 시작합니다 === (@help)')
+    print('=== 챗봇을 시작합니다 === (도움말: @help)')
     
     while True:
         user_input = input('\n입력:\n')
@@ -153,37 +164,44 @@ if __name__ == '__main__':
             print('\n입력이 비어 있습니다. 다시 입력해 주세요.')
             continue        
         
-        elif user_input in ['@quit', '@q']:
-            print('\n챗봇을 종료합니다')
-            break
-        
-        elif user_input in ['@clear', '@clr']:
-            memory.clear()
-            print('\n챗봇의 메모리를 삭제합니다')
-        
-        elif user_input == '@history':
-            chat_history = memory.load_memory_variables({}).get('chat_history', [])
-            if not chat_history:
-                print('\n대화 기록이 없습니다')
+        # 명령어
+        elif user_input[:1] == '@':
+            
+            if user_input in ['@quit', '@q']:
+                print('\n챗봇을 종료합니다')
+                break
+            
+            elif user_input in ['@clear', '@clr']:
+                memory.clear()
+                print('\n챗봇의 메모리를 삭제합니다')
+            
+            elif user_input == '@history':
+                chat_history = memory.load_memory_variables({}).get('chat_history', [])
+                if not chat_history:
+                    print('\n대화 기록이 없습니다')
+                else:
+                    print('\n=== 대화 기록 ===')
+                    for i, msg in enumerate(chat_history, 1):
+                        role = msg.type
+                        print(f'{role}: {msg.content}\n')
+            
+            elif user_input == '@update':
+                vectorstore = make_vectorstore()
+                print('\n자료를 업데이트합니다.')
+                
+            elif user_input == '@help':
+                print('\n=== 도움말 목록 ===')
+                print('- @quit, @q: 종료\n- @clear, @clr: 메모리 초기화\n- @history: 대화 기록\n- @update: 자료 업데이트')
+            
             else:
-                print('\n=== 대화 기록 ===')
-                for i, msg in enumerate(chat_history, 1):
-                    role = msg.type
-                    print(f'{role}: {msg.content}\n')
-        
-        elif user_input == '@update':
-            vectorstore = make_vectorstore()
-            print('\n자료를 업데이트합니다.')
-            
-        elif user_input == '@help':
-            print('\n=== 도움말 목록 ===')
-            print('- @quit, @q: 종료\n- @clear, @clr: 메모리 초기화\n- @history: 대화 기록\n- @update: 자료 업데이트')
-            
+                print('\n인식할 수 없는 명령어입니다. @help를 입력해 도움말을 불러오세요.')    
+                
         else:
             try:
                 print('\n답변:')
                 result = agent_executor.invoke({'input': user_input})
                 print('\n')
+                
             except Exception as e:
                 print(f'오류 발생: {e}')
                 continue
