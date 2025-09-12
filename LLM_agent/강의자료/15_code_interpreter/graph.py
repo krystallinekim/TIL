643a95e45f211eea29f1864b03cd9689ca8ce19c
krystallinekim <jskim11070@gmail.com>
2025-09-12 10:32:14 +0900

    최적화

--- LLM_agent/강의자료/15_code_interpreter/graph.py
+++ LLM_agent/강의자료/15_code_interpreter/graph.py
@@ -1,5 +1,4 @@
 from langgraph.graph import StateGraph, START, END
-# from langgraph.checkpoint.memory import InMemorySaver
 from state import State
 from nodes import generate_code, execute_code, save_code, generate_answer
 
@@ -9,7 +8,4 @@ builder.add_sequence([generate_code, execute_code, save_code, generate_answer])
 builder.add_edge(START, 'generate_code')
 builder.add_edge('generate_answer', END)
 
-
-# memory = InMemorySaver()
-# graph = builder.compile(checkpointer=memory)
 graph = builder.compile()
