f21f4681fe1a842c6a97b08ae9ac106b0ec0beb0
krystallinekim <jskim11070@gmail.com>
2026-01-23 14:49:30 +0900

    단항, 이항 연산자

---
+++ Java/02_Operator/src/com/beyond/operator/Application.java
@@ -0,0 +1,11 @@
+package com.beyond.operator;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("main\n");
+
+//        new InDecrease().method1();
+
+        
+    }
+}
---
+++ Java/02_Operator/src/com/beyond/operator/practice/InDecrease.java
@@ -0,0 +1,41 @@
+package com.beyond.operator.practice;
+
+public class InDecrease {
+    public void method1() {
+        System.out.println("Indecrease.method1\n");
+        int num;
+
+    // 전위 증감연산
+        num = 10;
+
+        System.out.println("전위연산 전 - " + num);
+        System.out.println("1회 실행 - " + ++num);
+        System.out.println("2회 실행 - " + ++num);
+        System.out.println("3회 실행 - " + ++num);
+        System.out.println("전위연산 후 - " + num);
+        System.out.println();
+
+
+    // 후위 증감연산
+        System.out.println("후위연산 전 - " + num);
+        System.out.println("1회 실행 - " + num--);
+        System.out.println("2회 실행 - " + num--);
+        System.out.println("3회 실행 - " + num--);
+        System.out.println("후위연산 후 - " + num);
+        System.out.println();
+
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+
+        // 전/후위연산에서 괄호는 의미 없음
+        System.out.println(num1++);  // 10, num1 = 11
+        System.out.println((++num1) + (num2++));  // 12 + 20 = 32, num1 = 12, num2 = 21
+        System.out.println((num1++) + (--num2) + (--num3));  // 12 + 20 + 29 = 61, num1 = 13, num2 = 20, num3 = 29
+
+        System.out.println(num1);  // 13
+        System.out.println(num2);  // 20
+        System.out.println(num3);  // 29
+
+    }
+}
