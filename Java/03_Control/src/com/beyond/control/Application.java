31f4f7d047f7e30d49aeda6c891fd8c662ebf16a
krystallinekim <jskim11070@gmail.com>
2026-01-26 17:22:41 +0900

    수업내용 정리

---
+++ Java/03_Control/src/com/beyond/control/Application.java
@@ -0,0 +1,32 @@
+package com.beyond.control;
+
+import com.beyond.control.practice.Branch;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("main\n");
+
+//        new If().method1();
+//        new If().method2();
+//        new If().method3();
+//        new If().practice1();
+
+//        new Switch().method1();
+//        new Switch().method2();
+//        new Switch().practice1();
+
+//        new For().method1();
+//        new For().method2();
+//        new For().practice1();
+
+//        new While().method1();
+//        new While().method2();
+//        new While().method3();
+//        new While().method4();
+
+//        new Branch().method1();
+        new Branch().method2();
+    }
+
+
+}
