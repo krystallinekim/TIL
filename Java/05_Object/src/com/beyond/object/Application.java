4facb3d38eacd07062f236f178b67740e3364f1b
krystallinekim <jskim11070@gmail.com>
2026-01-27 17:28:14 +0900

    필드

--- Java/05_Object/src/com/beyond/object/Application.java
+++ Java/05_Object/src/com/beyond/object/Application.java
@@ -1,7 +1,24 @@
 package com.beyond.object;
 
+import com.beyond.object.practice.Person;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        // Hong이라는 객체를 생성(생성자를 호출한다)
+        Person Hong = new Person();  // 클래스명 변수명 = new 생성자(없으면 자동 생성됨)
+
+        Hong.setName("홍길동"); // 매개값을 전달
+        Hong.setAge(34);
+        Hong.whoAreYou();
+
+        // Lee라는 객체를 생성
+        Person Lee = new Person();
+        Lee.setName("이몽룡");
+        Lee.setAge(24);
+        Lee.whoAreYou();
+
+        // 하나의 클래스로 사람을 둘 만들었음
+        // 캡슐화되어 다른 객체에 다른 이름/나이로 존재함
     }
 }
--- Java/05_Object/src/com/beyond/object/practice/Person.java
+++ Java/05_Object/src/com/beyond/object/practice/Person.java
@@ -1,5 +1,36 @@
 package com.beyond.object.practice;
 
+/*
+클래스에서 사용 가능한 접근 제한자 (클래스는 대부분 public)
+- public: 제약 없음. public 제한자는 한 클래스 파일에 하나만 선언 가능
+- default: 이런 접근제한자는 없다. 지워놓으면 default
+ */
+
 public class Person {
-    
+    // 1. field: 객체의 속성, 데이터
+    // 클래스 내에서는 어디에서나 접근 가능하다(생성자나 메소드에서도)
+    private String name;
+    private int age;
+    // 변수랑 똑같이 만들어지고, 생긴거도 똑같지만 변수가 아니다
+    // 주로 private를 걸고 만드는데, 이 클래스 내부에서만 접근 가능하도록
+
+    // 2. constructor(생성자)
+    // 클래스의 이름과 생성자 이름은 같다
+    // 생긴건 메소드처럼 생김
+    public Person() {
+        System.out.println("생성자 호출");
+    }
+
+    // 3. 메소드: 객체의 기능, 동작 -> 이름이 동사형
+    public void whoAreYou() {
+        System.out.printf("저는 %s이고, %d세입니다.\n", name, age);  // field에서 가져옴
+    }
+
+    // 여기서 String n, int a는 매개변수이다
+    public void setName(String n) {
+        name = n;
+    }
+    public void setAge(int a) {
+        age = a;
+    }
 }
