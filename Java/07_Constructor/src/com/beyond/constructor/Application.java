e51880b0aaeb3f6b0e12716962575d4b30c0ff7f
krystallinekim <jskim11070@gmail.com>
2026-01-28 15:10:49 +0900

    this.와 this()

--- Java/07_Constructor/src/com/beyond/constructor/Application.java
+++ Java/07_Constructor/src/com/beyond/constructor/Application.java
@@ -4,11 +4,21 @@ import com.beyond.constructor.practice.User;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("생성자\n");
 
+        // 기본 생성자로 생성한 객체
         User user1 = new User();  // 기본 생성자(안만들어도 자동으로 생성 / 다른 생성자를 선언하면 자동생성 안함)
+        // System.out.println(user1);
         System.out.println(user1.information());
 
+        // 매개변수 3개로 생성한 객체
         User user2 = new User("Hong123", "password1234", "홍길동");
+        // System.out.println(user2);
         System.out.println(user2.information());
+
+        // 매개변수 5개로 생성한 객체
+        User user3 = new User("Lee321", "password1111", "이몽룡", 24, 'M');
+        // System.out.println(user3);
+        System.out.println(user3.information());
     }
 }
--- Java/07_Constructor/src/com/beyond/constructor/practice/User.java
+++ Java/07_Constructor/src/com/beyond/constructor/practice/User.java
@@ -21,8 +21,21 @@ public class User {
         // 나머지 필드는 기본 초기값
     }
 
+    // 서로 다른 생성자를 구분하는 기준은 매개변수의 타입과 개수
+    public User(String id, String password, String name, int age, char gender) {
+        this(id, password, name);  // 생성자 내부에서 다른 생성자 호출
+        // this();  // 매개변수 없는 생성자가 있어서 이거도 에러는 안남.
+        // this.id = id;
+        // this.password = password;
+        // this.name = name;  // id, pw, name은 위의 생성자와 중복된다
+        this.age = age;
+        this.gender = gender;
+    }
+
     // 메소드
     public String information() {
+        // System.out.println(this);  // this = 객체 자기 자신 참조하는 변수
+        // this("Seong12", "password1111", "성춘향")  // 메소드 내부에서는 호출 불가
         return String.format(
                 "id: %s\npassword: %s\nname: %s\nage: %d\ngender: %c\n",
                 id, password, name, age, gender
