ae98556ee2d83141af946a548ffd8bff8df5a552
krystallinekim <jskim11070@gmail.com>
2026-03-11 17:46:56 +0900

    log4j 적용

--- Spring/03_SpringMVC/src/main/java/com/beyond/mvc/homeController.java
+++ Spring/03_SpringMVC/src/main/java/com/beyond/mvc/homeController.java
@@ -1,14 +1,21 @@
 package com.beyond.mvc;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
-public class homeController {
+public class HomeController {
+    // @Slf4j(lombok)을 쓰면 다음 선언은 자동으로 해준다.
+    // private static final Logger log = LoggerFactory.getLogger(HomeController.class);
 
     @RequestMapping("/")
     public String home() {
 
+        log.info("home() 메소드 호출 [INFO ]");
+        log.debug("home() 메소드 호출 [DEBUG]");
+        log.error("home() 메소드 호출 [ERROR]");
         // 뷰의 이름
         return "home";
     }
